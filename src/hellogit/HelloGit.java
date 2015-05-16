06315336de3c1b374a75f6c267c053a704a3e1d6
VPZabluda <vpzabluda@gmail.com>
2015-05-16 22:21:05 +0300

    Svetlana added

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -15,7 +15,7 @@ public class HelloGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     System.out.println("Hello from Bob !");
+     System.out.println("Hello from Bob and Svetlana !");
     }
     
 }
